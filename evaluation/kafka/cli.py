030da3e9646edc9e48326a4582c84c523449413c
valhayot <valeriehayot@gmail.com>
2024-01-02 09:34:29 -0500

    Fix for benchmarks

--- evaluation/kafka/cli.py
+++ evaluation/kafka/cli.py
@@ -8,14 +8,19 @@ from random import randint
 from random import choices
 from time import sleep
 from time import time_ns
+from typing import Literal
 from typing import TextIO
 
+from diaspora_event_sdk import block_until_ready
+from diaspora_event_sdk import Client as GlobusClient
 from globus_compute_sdk import Executor
 
 from evaluation.kafka.stream import Consumer
 from evaluation.kafka.stream import Producer
 
-def run_kafka_consumer(topic: str, bootstrap_servers: str, port:str) -> str:
+FRAMEWORK = Literal['kafka', 'diaspora']
+
+def run_kafka_consumer(topic: str, bootstrap_servers: str, port: str, num_events: int, framework: FRAMEWORK = 'kafka', proxystore: bool = False) -> str:
     import logging
     import sys
 
@@ -24,128 +29,192 @@ def run_kafka_consumer(topic: str, bootstrap_servers: str, port:str) -> str:
 
     from evaluation.kafka.stream import Consumer
 
-    sleep(1)
-    c = Consumer(topic = topic, bootstrap_servers=bootstrap_servers, port=port)
-    logging.debug('consumer started')
+    c = Consumer(topic=topic, bootstrap_servers=bootstrap_servers, port=port, framework=framework, proxy=proxystore)
+    logging.debug("consumer started")
 
     output: str = ""
-    for e,v in c:
+    for i in range(num_events):
         start = time_ns()
-        logging.debug(e)
+        e,v = next(c)
+        #print(v)
         end = time_ns()
         duration = end - start
 
-        print(f'Received event {e} in {end - start} nanoseconds with data {v}')
-        output += f'kafka,{e["event"]},consume,{len(v)},{duration}\n' 
+        #print(f"Received event {e} in {end - start} nanoseconds with data {e}")
+        output += f'{framework},{e["event"]},consume,{len(v)},{duration}\n'
     return output
 
+
 def run_mofka_consumer(num_events: int, benchmark_file: str):
-    import subprocess 
+    import subprocess
 
     s = subprocess.run(
         [
-            '/Users/valeriehayot-sasson/postdoc/proxystream/evaluation/mofka/build/consumer',
-            '-p',
-            'ofi+tcp',
-            '-s',
-            'mofka.ssg',
-            '-e',
+            "/Users/valeriehayot-sasson/postdoc/proxystream/evaluation/mofka/build/consumer",
+            "-p",
+            "ofi+tcp",
+            "-s",
+            "mofka.ssg",
+            "-e",
             str(num_events),
-            '-f',
+            "-f",
             benchmark_file
         ],
         check=True,
-        capture_output=True
+        capture_output=True,
     )
     print(s.stdout, s.stderr)
     return s.stdout
 
-    
 
-def launch_kafka(bf:str, endpoint:str, broker: str, port:int, num_events:int, size: int):
+def launch_kafka(
+    bf: str, endpoint: str, topic:str, broker: str, port: int, num_events: int, size: int, framework: FRAMEWORK = 'kafka', proxystore: bool = False
+):
     f: TextIO | None = None
 
     if bf is not None:
-        f = open(bf, 'a+')
-    
-    # start consumer before producer to get events as producer is producing them 
-    with Executor(endpoint_id=endpoint) as gce:    
-        future = gce.submit(run_kafka_consumer, topic, broker, port)
+        f = open(bf, "a+")
 
-        p = Producer(bootstrap_servers=broker, port=port)
+    # start consumer before producer to get events as producer is producing them
+    with Executor(endpoint_id=endpoint) as gce:
+        future = gce.submit(run_kafka_consumer, topic=topic, bootstrap_servers=broker, port=port, num_events=num_events, framework=framework, proxystore=proxystore)
+
+        p = Producer(bootstrap_servers=broker, port=port, framework=framework, proxy=proxystore)
+
         p.create_topic(topic)
 
         for i in range(num_events):
             start = time_ns()
-            data = ''.join(choices(string.ascii_uppercase, k=size))
+            data = "".join(choices(string.ascii_uppercase, k=size))
             p.publish(topic, data)
             end = time_ns()
             duration = end - start
 
-            print(f'Published data {data} in {duration} nanoseconds')
+            print(f"Published data {data} in {duration} nanoseconds")
 
             if f is not None:
-                f.write(f'kafka,{i},publish,{len(data)},{duration}\n')
+                f.write(f"{framework},{i},publish,{len(data)},{duration}\n")
 
         if f is not None:
             f.write(future.result())
 
+        if p.store is not None:
+            p.store.close()
+
     if f is not None:
         f.close()
 
-if __name__=='__main__':
 
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-                    prog='Kafka (proxy) Stream',
-                    description='Transmits proxied data as a stream using Kafka',
-                )
+        prog="Kafka (proxy) Stream",
+        description="Transmits proxied data as a stream using Kafka",
+    )
 
-    parser.add_argument('type', choices=['kafka', 'diaspora', 'mofka'], help='Stream framework type')
-    
-    parser.add_argument('-e', '--endpoint', type=str, help='Globus Compute Endpoint UUID')
-    parser.add_argument('-s', '--size', type=int, default=8, help='Data size')
-    parser.add_argument('-n', '--num-events', type=int, default=10, help='Number of events to transmit')
-    parser.add_argument('-p', '--port', type=int, default=randint(40000, 70000), help='Port to use for MargoConnector')
-    parser.add_argument('-b', '--broker', type=str, default='0.0.0.0:9092', help='Kafka broker address')
-    parser.add_argument('-l', '--log-level', choices=['DEBUG', 'INFO', 'WARNING', 'ERROR'], default='ERROR')
-    parser.add_argument('-f', '--benchmark-file', type=str, default=None, help='File to save benchmark data to (will print to console otherwise)')
+    parser.add_argument(
+        "type", choices=["kafka", "diaspora", "mofka"], help="Stream framework type"
+    )
+
+    parser.add_argument(
+        "-e", "--endpoint", type=str, help="Globus Compute Endpoint UUID"
+    )
+    parser.add_argument("-s", "--size", type=int, default=8, help="Data size")
+    parser.add_argument(
+        "-n", "--num-events", type=int, default=10, help="Number of events to transmit"
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        default=randint(40000, 70000),
+        help="Port to use for MargoConnector",
+    )
+    parser.add_argument(
+        "-b", "--broker", type=str, default="0.0.0.0:9092", help="Kafka broker address"
+    )
+    parser.add_argument(
+        "-l",
+        "--log-level",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR"],
+        default="ERROR",
+    )
+    parser.add_argument(
+        "-f",
+        "--benchmark-file",
+        type=str,
+        default=None,
+        help="File to save benchmark data to (will print to console otherwise)",
+    )
+    parser.add_argument(
+        "-ps",
+        "--proxystore",
+        action='store_true',
+        help='Store data within ProxyStore'
+    )
 
     args = parser.parse_args()
-    
-    level = logging.getLevelName(args.log_level)    
+
+    level = logging.getLevelName(args.log_level)
     logging.basicConfig(level=level, handlers=[logging.StreamHandler()])
 
-    topic = 'stream-1'
+    topic = "stream-1"
 
-    if args.type == 'kafka': 
+    if args.proxystore is True:
+        framework = f'{args.type}-proxystore'
+    else:
+        framework = f'{args.type}-noproxy'
+
+    if args.type == "kafka":
         launch_kafka(
             bf=args.benchmark_file,
             endpoint=args.endpoint,
+            topic=topic,
             broker=args.broker,
             port=args.port,
             num_events=args.num_events,
-            size=args.size
+            size=args.size,
+            framework=framework,
+            proxystore=args.proxystore
         )
-    elif args.type == 'mofka':
+    elif args.type == "mofka":
         # run producer
         s = subprocess.run(
             [
-                './evaluation/mofka/build/producer',
-                '-p',
-                'ofi+tcp',
-                '-s',
-                'mofka.ssg',
-                '-e',
+                "./evaluation/mofka/build/producer",
+                "-p",
+                "ofi+tcp",
+                "-s",
+                "mofka.ssg",
+                "-e",
                 str(args.num_events),
-                '-f',
-                args.benchmark_file
+                "-f",
+                args.benchmark_file,
             ],
             check=True,
-            capture_output=True
+            capture_output=True,
         )
-        
+
         print(s.stdout)
-        
-        with Executor(endpoint_id=args.endpoint) as gce:    
-            future = gce.submit(run_mofka_consumer, args.num_events, args.benchmark_file)
+
+        with Executor(endpoint_id=args.endpoint) as gce:
+            future = gce.submit(
+                run_mofka_consumer, args.num_events, args.benchmark_file
+            )
             _ = future.result()
+    else:
+        c = GlobusClient()
+        c.register_topic(topic)
+        assert block_until_ready()
+        print('globus client started')
+        launch_kafka(
+            bf=args.benchmark_file,
+            endpoint=args.endpoint,
+            topic=topic,
+            broker=args.broker,
+            port=args.port,
+            num_events=args.num_events,
+            size=args.size,
+            framework=framework,
+            proxystore=args.proxystore
+        )
+        c.unregister_topic(topic)
--- evaluation/kafka/stream.py
+++ evaluation/kafka/stream.py
@@ -8,72 +8,123 @@ from random import randint
 from random import random
 from time import time_ns
 from typing import Any
+from typing import Iterator
+from typing import Literal
 from typing import TextIO
+
+
+from diaspora_event_sdk import KafkaProducer as DiasporaProducer
+from diaspora_event_sdk import KafkaConsumer as DiasporaConsumer
 from kafka import KafkaConsumer
 from kafka import KafkaProducer
 
 from proxystore_ex.connectors.dim.margo import MargoConnector
-from proxystore_ex.connectors.dim.margo import Protocol 
+from proxystore_ex.connectors.dim.margo import Protocol
 from proxystore.store import Store
 from proxystore.store.utils import get_key
 
+FRAMEWORK = Literal['kafka', 'diaspora']
+
 class Producer:
-    p: KafkaProducer
-    store: Store[MargoConnector]
+    p: KafkaProducer | DiasporaProducer
     topics: dict[str, int]
-    
-    def __init__(self, port: int, bootstrap_servers: str = '0.0.0.0:9092'):
-        connector = MargoConnector(port=port, protocol=Protocol.OFI_TCP)
-        self.store = Store(name='kafka_store', connector=connector)
-        self.p = KafkaProducer(bootstrap_servers = bootstrap_servers)
+    store: Store[MargoConnector] | None = None
+
+    def __init__(
+        self,
+        port: int,
+        bootstrap_servers: str = "0.0.0.0:9092",
+        framework: FRAMEWORK = "kafka",
+        proxy: bool = False
+    ):
+        if proxy is True:
+            connector = MargoConnector(port=port, protocol=Protocol.OFI_TCP)
+            self.store = Store(name="kafka_store", connector=connector)
+
+        if "kafka" in framework:
+            self.p = KafkaProducer(bootstrap_servers=bootstrap_servers)
+        else:
+            self.p = DiasporaProducer()
+
         self.topics = {}
-        logging.debug('Producer initialized')
-        
+        logging.debug("Producer initialized")
+
     def create_topic(self, name: str):
         self.topics[name] = 0
 
     @classmethod
     def serialize(cls, key: dict):
         s = json.dumps(key)
-        return s.encode('utf-8')
-        
-    def publish(self, topic:str, value: Any):
-        proxy = self.store.proxy(value)
+        return s.encode("utf-8")
 
+    def publish(self, topic: str, value: Any):
         self.topics[topic] += 1
-        event_key = { 'event': self.topics[topic], 'metadata': get_key(proxy)._asdict()}
+        if self.store is not None:
+            proxy = self.store.proxy(value)
+            event_key = {"event": self.topics[topic], "metadata": get_key(proxy)._asdict()}
+            value = self.store.serializer(proxy)
 
+        else:
+            event_key = {"event": self.topics[topic]}
+            value = value.encode('utf-8')
+
+        print(topic, self.serialize(event_key), value)
         self.p.send(
             topic=topic,
             key=self.serialize(event_key),
-            value=self.store.serializer(proxy)
+            value=value,
         )
 
-        if self.topics[topic] % 100 == 0:
+        if self.topics[topic] % 1 == 0:
             self.p.flush()
-        logging.info(f'Event {event_key} submitted')
+        logging.info(f"Event {event_key} submitted")
+
 
 class Consumer:
-    c: KafkaConsumer
+    c: KafkaConsumer | DiasporaConsumer
     topic: str
     event_idx: int
-    store: Store[MargoConnector]
+    _iterator : Iterator
+    store: Store[MargoConnector] | None = None
 
-    def __init__(self, port:int, topic:str, bootstrap_servers: str = '0.0.0.0:9092', consumer_timeout_ms: int = 1000) -> None:
-        connector = MargoConnector(port=port, protocol=Protocol.OFI_TCP)
-        self.store = Store(name='kafka_store', connector=connector)
+    def __init__(
+        self,
+        port: int,
+        topic: str,
+        bootstrap_servers: str = "0.0.0.0:9092",
+        consumer_timeout_ms: int = 1000,
+        framework: FRAMEWORK = 'kafka',
+        proxy: bool = False
+    ) -> None:
+        
+        if proxy is True:
+            connector = MargoConnector(port=port, protocol=Protocol.OFI_TCP)
+            self.store = Store(name="kafka_store", connector=connector)
+
         self.topic = topic
-        self.c = KafkaConsumer(self.topic, bootstrap_servers=bootstrap_servers, consumer_timeout_ms=consumer_timeout_ms)
+
+        if 'kafka' in framework:
+            self.c = KafkaConsumer(
+                self.topic,
+                bootstrap_servers=bootstrap_servers,
+                consumer_timeout_ms=consumer_timeout_ms,
+                auto_offset_reset='earliest'
+            )
+        else:
+            self.c = DiasporaConsumer(self.topic, consumer_timeout_ms=consumer_timeout_ms, auto_offset_reset='earliest')
+
         self.event_idx = 1
 
     def __iter__(self):
         return self
     
     def __next__(self):
-        if (event := next(self.c)) is not None:
-            return self.deserialize(event.key), self.store.deserializer(event.value) # f'{self.deserialize(event.key)} with data {self.store.deserializer(event.value)}'
-    
+        e = next(self.c)
+        if self.store is not None:
+            return (self.deserialize(e.key), self.store.deserializer(e.value))
+        return (self.deserialize(e.key), e.value.decode('utf-8'))
+
     @classmethod
     def deserialize(cls, key: bytes):
-        s = key.decode('utf-8')
+        s = key.decode("utf-8")
         return json.loads(s)
